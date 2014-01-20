575becfd56e2de174d96fe2e6ebe7f5667ec8730
Mantas Norvaiša <mntnorv@gmail.com>
2014-01-20 17:46:37 +0200

    Added a stub Dictionary class

---
+++ wrdl/src/main/java/com/mntnorv/wrdl/dict/Dictionary.java
@@ -0,0 +1,21 @@
+package com.mntnorv.wrdl.dict;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+    private List<byte[]> words;
+
+    public Dictionary(InputStream stream) {
+        BufferedInputStream bufStream = new BufferedInputStream(stream);
+        words = new ArrayList<byte[]>(200000);
+
+        readWords(bufStream);
+    }
+
+    private void readWords(BufferedInputStream stream) {
+
+    }
+}
