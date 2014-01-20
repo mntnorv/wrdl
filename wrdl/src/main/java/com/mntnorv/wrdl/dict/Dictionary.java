85a3e20116d3e98cc7406262988361b7e79cae8b
Mantas Norvaiša <mntnorv@gmail.com>
2014-01-20 18:55:08 +0200

    Testing dictionary read

--- wrdl/src/main/java/com/mntnorv/wrdl/dict/Dictionary.java
+++ wrdl/src/main/java/com/mntnorv/wrdl/dict/Dictionary.java
@@ -1,6 +1,10 @@
 package com.mntnorv.wrdl.dict;
 
+import android.util.Log;
+
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +12,28 @@ import java.util.List;
 public class Dictionary {
 	private List<byte[]> words;
 
-	public Dictionary(InputStream stream) {
+	public Dictionary(InputStream stream) throws IOException {
 		BufferedInputStream bufStream = new BufferedInputStream(stream);
 		words = new ArrayList<byte[]>(200000);
 
 		readWords(bufStream);
 	}
 
-	private void readWords(BufferedInputStream stream) {
+	private void readWords(BufferedInputStream inputStream) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+		int byteRead;
+		while ((byteRead = inputStream.read()) != -1) {
+			if (byteRead != '\n') {
+				outputStream.write(byteRead);
+			} else {
+				words.add(outputStream.toByteArray());
+				outputStream.reset();
+			}
+		}
+
+		Log.d("Dictionary", Integer.toString(words.size()));
+
+		outputStream.close();
 	}
 }
