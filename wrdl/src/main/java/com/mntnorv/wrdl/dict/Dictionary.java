1edabf6247f4124cb5a6b12dae5ced9edfeb1474
Mantas Norvaiša <mntnorv@gmail.com>
2014-01-20 19:50:15 +0200

    Added contains methods to dictionary

--- wrdl/src/main/java/com/mntnorv/wrdl/dict/Dictionary.java
+++ wrdl/src/main/java/com/mntnorv/wrdl/dict/Dictionary.java
@@ -7,14 +7,16 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Dictionary {
-	private List<byte[]> words;
+	private List<byte[]> mWordList;
 
 	public Dictionary(InputStream stream) throws IOException {
 		BufferedInputStream bufStream = new BufferedInputStream(stream);
-		words = new ArrayList<byte[]>(200000);
+		mWordList = new ArrayList<byte[]>(200000);
 
 		readWords(bufStream);
 	}
@@ -27,13 +29,64 @@ public class Dictionary {
 			if (byteRead != '\n') {
 				outputStream.write(byteRead);
 			} else {
-				words.add(outputStream.toByteArray());
+				mWordList.add(outputStream.toByteArray());
 				outputStream.reset();
 			}
 		}
 
-		Log.d("Dictionary", Integer.toString(words.size()));
+		Log.d("Dictionary", Integer.toString(mWordList.size()));
 
 		outputStream.close();
 	}
+
+	private byte[] encodeWord(String word) {
+		byte[] encoded = new byte[word.length()];
+
+		for (int i = 0; i < word.length(); i++) {
+			encoded[i] = (byte) word.charAt(i);
+		}
+
+		return encoded;
+	}
+
+	private String decodeWord(byte[] word) {
+		StringBuilder builder = new StringBuilder();
+
+		for (int i = 0; i < word.length; i++) {
+			builder.append((char) word[i]);
+		}
+
+		return builder.toString();
+	}
+
+	public boolean contains(String word) {
+		return Collections.binarySearch(mWordList, encodeWord(word), dictComparator) >= 0;
+	}
+
+	public boolean containsPrefix (String prefix) {
+		int result = Collections.binarySearch(mWordList, encodeWord(prefix), dictComparator);
+
+		if (result >= 0) {
+			return true;
+		} else if ((-result-1) < mWordList.size()) {
+			return decodeWord(mWordList.get(-result-1)).startsWith(prefix);
+		} else {
+			return false;
+		}
+	}
+
+	private Comparator<byte[]> dictComparator = new Comparator<byte[]> () {
+		@Override
+		public int compare(byte[] lhs, byte[] rhs) {
+			int minLength = lhs.length > rhs.length ? rhs.length : lhs.length;
+
+			for (int i = 0; i < minLength; i++) {
+				if (lhs[i] != rhs[i]) {
+					return lhs[i] - rhs[i];
+				}
+			}
+
+			return lhs.length - rhs.length;
+		}
+	};
 }
