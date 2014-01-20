f02f75b8e213af832c9a2764727cae3d46de7711
Mantas Norvaiša <mntnorv@gmail.com>
2014-01-20 19:29:20 +0200

    Moved dictionary loading to the background

--- wrdl/src/main/java/com/mntnorv/wrdl/GameFragment.java
+++ wrdl/src/main/java/com/mntnorv/wrdl/GameFragment.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mntnorv.wrdl.dict.Dictionary;
 import com.mntnorv.wrdl.dict.DictionaryProvider;
+
+import rx.Observer;
+import rx.concurrency.Schedulers;
 
 public class GameFragment extends Fragment {
 	public GameFragment() {
@@ -17,8 +21,22 @@ public class GameFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_game, container, false);
 
-		DictionaryProvider.getDictionary(getActivity(), R.raw.sowpods3);
+		DictionaryProvider.getDictionary(getActivity(), R.raw.sowpods3)
+				.subscribeOn(Schedulers.newThread())
+				.observeOn(Schedulers.currentThread())
+				.subscribe(new DictionaryObserver());
 
 		return rootView;
+	}
+
+	private class DictionaryObserver implements Observer<Dictionary> {
+		@Override
+		public void onNext(Dictionary dictionary) {}
+
+		@Override
+		public void onError(Throwable throwable) {}
+
+		@Override
+		public void onCompleted() {}
 	}
 }
--- wrdl/src/main/java/com/mntnorv/wrdl/dict/DictionaryProvider.java
+++ wrdl/src/main/java/com/mntnorv/wrdl/dict/DictionaryProvider.java
@@ -1,34 +1,46 @@
 package com.mntnorv.wrdl.dict;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.subscriptions.Subscriptions;
+import rx.util.functions.Func1;
 
 public class DictionaryProvider {
 	private static Dictionary dict;
 	private static int mOpenedDict = 0;
 
-	public static Dictionary getDictionary(Context context, int resourceId) {
-		if (mOpenedDict != resourceId) {
-			dict = openDictionary(context, resourceId);
-		}
+	public static Observable<Dictionary> getDictionary(final Context context, final int resourceId) {
+		return Observable.create(new Func1<Observer<Dictionary>, Subscription>() {
+			@Override
+			public Subscription call(Observer<Dictionary> dictionaryObserver) {
 
-		return dict;
+				try {
+					if (mOpenedDict != resourceId) {
+						dict = openDictionary(context, resourceId);
+					}
+
+					dictionaryObserver.onNext(dict);
+					dictionaryObserver.onCompleted();
+				} catch (IOException e) {
+					dictionaryObserver.onError(e);
+				}
+
+				return Subscriptions.empty();
+			}
+		});
 	}
 
-	private static Dictionary openDictionary(Context context, int resourceId) {
-		try {
-			InputStream stream = context.getResources().openRawResource(resourceId);
-			Dictionary dict = new Dictionary(stream);
-			stream.close();
-			mOpenedDict = resourceId;
-			return dict;
-		} catch (IOException e) {
-			Log.e("DictionaryProvider", "unable to open dictionary from resource " + Integer.toString(resourceId));
-			mOpenedDict = 0;
-			return null;
-		}
+	private static Dictionary openDictionary(Context context, int resourceId) throws IOException {
+		InputStream stream = context.getResources().openRawResource(resourceId);
+		Dictionary dict = new Dictionary(stream);
+		stream.close();
+		mOpenedDict = resourceId;
+		return dict;
 	}
 }
